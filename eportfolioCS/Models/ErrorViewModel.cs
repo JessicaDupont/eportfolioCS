f705f260b6fa4d2d272cbf4cd14b05c8a1096755
Jessica DUPONT <jessica.dupont1990@gmail.com>
2022-09-24 16:37:13 +0200

    Ajoutez des fichiers projet.

---
+++ eportfolioCS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eportfolioCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
